92f7ae4304cc0536327d7547b28553c975aac130
Jonatas Kalebe <jonathas.kbe@gmail.com>
2026-08-04 22:56:10 -0300

    adicionando github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  projectId: "r2ja89",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://petlov.vercel.app'
+    baseUrl: "https://petlov.vercel.app",
   },
 };
